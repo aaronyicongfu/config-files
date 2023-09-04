6452ac4e506ee4483bb23efacf1169f4ea85a549
Yicong Fu <fuyicong1996@gmail.com>
2023-09-04 13:55:32 -0400

    double qoute -> single qoute

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,21 +1,21 @@
 -- Set up the package manager for LSP servers
-require("mason").setup()
+require('mason').setup()
 
 
 -- Ensure servers are there, if not, install them using mason
-require("mason-lspconfig").setup({
+require('mason-lspconfig').setup({
   -- install language servers
-  ensure_installed = {"lua_ls", "pyright", "clangd"},
+  ensure_installed = {'lua_ls', 'pyright', 'clangd'},
 })
 
 -- Ensure formatters are there, if not, install them using mason
 -- Set up formatters
-require("mason-null-ls").setup({
-    ensure_installed = {"black", "clang-format"}
+require('mason-null-ls').setup({
+    ensure_installed = {'black', 'clang-format'}
 })
 
 -- Set up language servers
-local lspconfig = require("lspconfig")
+local lspconfig = require('lspconfig')
 lspconfig.lua_ls.setup{}
 lspconfig.pyright.setup{}
 lspconfig.clangd.setup{}
@@ -23,13 +23,13 @@ lspconfig.clangd.setup{}
 
 -- Create an autocmd group for (fos) formatting-on-save
 -- ref: https://www.youtube.com/watch?v=4BnVeOUeZxc
-local fos_augroup = vim.api.nvim_create_augroup("LspFormatting", {}) local fos_on_attach = function(client, bufnr)
-  if client.supports_method("textDocument/formatting") then
+local fos_augroup = vim.api.nvim_create_augroup('LspFormatting', {}) local fos_on_attach = function(client, bufnr)
+  if client.supports_method('textDocument/formatting') then
     vim.api.nvim_clear_autocmds({
       group = fos_augroup,
       buffer = bufnr,
     })
-    vim.api.nvim_create_autocmd("BufWritePre", {
+    vim.api.nvim_create_autocmd('BufWritePre', {
       group = fos_augroup,
       buffer = bufnr,
       callback = function()
@@ -40,27 +40,27 @@ local fos_augroup = vim.api.nvim_create_augroup("LspFormatting", {}) local fos_o
 end
 
 -- Set up formatters
-local null_ls = require("null-ls")
+local null_ls = require('null-ls')
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.black,
-    null_ls.builtins.formatting.clang_format.with({extra_args={"-style=Google"}}),
+    null_ls.builtins.formatting.clang_format.with({extra_args={'-style=Google'}}),
   },
   on_attach = fos_on_attach
 })
 
 -- Set up autocompletion
 -- ref: https://www.youtube.com/watch?v=_DnmphIwnjo
-local cmp = require("cmp")
+local cmp = require('cmp')
 cmp.setup({
   -- order is the automatic ranking for sources to show up!
   -- you can configure: 
   --   max_item_count
   --   priority
   sources = {
-    {name = "nvim_lua"},
-    {name = "nvim_lsp"},
-    {name = "path"},
-    {name = "buffer", keyword_length = 5},
+    {name = 'nvim_lua'},
+    {name = 'nvim_lsp'},
+    {name = 'path'},
+    {name = 'buffer', keyword_length = 5},
   },
 })
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,57 +1,75 @@
 -- Install lazy.nvim the plugin manager, if not already installed
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
     lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)  -- add to neovim's runtime path
 
-vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+vim.g.mapleader = ' ' -- Make sure to set `mapleader` before lazy so your mappings are correct
 
 -- Specify plugins
 local plugins = {
-    "navarasu/onedark.nvim",  -- color schemes, TODO: maybe try this: projekt0n/github-nvim-theme
-    "nvim-lua/plenary.nvim",  -- seems to be a collection of lua utility functions
+    'navarasu/onedark.nvim',  -- color schemes, TODO: maybe try this: projekt0n/github-nvim-theme
+    'nvim-lua/plenary.nvim',  -- seems to be a collection of lua utility functions
     {
-        "nvim-telescope/telescope.nvim",  -- fuzzy finder
+        'nvim-telescope/telescope.nvim',  -- fuzzy finder
         dependencies = { 'nvim-lua/plenary.nvim' },
     },
 
     -- below three work together for lsp functionality
-    "neovim/nvim-lspconfig",         -- set up config so that neovim talks to lsp
-    "williamboman/mason.nvim",            -- package manager to install lsp servers
-    "williamboman/mason-lspconfig.nvim",  -- bridge the gap between mason and lspconfig
+    'neovim/nvim-lspconfig',         -- set up config so that neovim talks to lsp
+    'williamboman/mason.nvim',            -- package manager to install lsp servers
+    'williamboman/mason-lspconfig.nvim',  -- bridge the gap between mason and lspconfig
 
     -- below two work together and use language servers for formatter functionality
-    "jose-elias-alvarez/null-ls.nvim",
-    "jay-babu/mason-null-ls.nvim",
+    'jose-elias-alvarez/null-ls.nvim',
+    'jay-babu/mason-null-ls.nvim',
 
     -- autocompletion!
-    "hrsh7th/nvim-cmp",  -- nvim-cmp core
-    "hrsh7th/cmp-buffer",  -- complete from buffer
-    "hrsh7th/cmp-path",  -- complete file
-    "hrsh7th/cmp-nvim-lua",  -- complete lua
-    "hrsh7th/cmp-nvim-lsp",  -- talks to lsp
-    "saadparwaiz1/cmp_luasnip",  -- create lua snippets (TODO: find a similar plugin for cpp)
+    'hrsh7th/nvim-cmp',  -- nvim-cmp core
+    'hrsh7th/cmp-buffer',  -- complete from buffer
+    'hrsh7th/cmp-path',  -- complete file
+    'hrsh7th/cmp-nvim-lua',  -- complete lua
+    'hrsh7th/cmp-nvim-lsp',  -- talks to lsp
+    'saadparwaiz1/cmp_luasnip',  -- create lua snippets (TODO: find a similar plugin for cpp)
 
     -- git integration
-    "lewis6991/gitsigns.nvim",
+    'lewis6991/gitsigns.nvim',
+
+    -- File explorer and icons
+    'nvim-tree/nvim-tree.lua',
+    'nvim-tree/nvim-web-devicons',
+
+    -- Hightlight word under cursor (TODO: find a useable plugin)
 }
 
 -- Specify options
 local opts = {
-  ui = {icons = {start = "▷ "}}
+  ui = {icons = {start = '▷ '}}
 }  -- original start icon is a question mark
 
 -- Set up plugins
-require("lazy").setup(plugins, opts)
+require('lazy').setup(plugins, opts)
 
--- Set up gitsigns
-require("gitsigns").setup()
+-- =========================
+-- Set up individual plugins
+-- =========================
+
+require('gitsigns').setup()
+
+-- disable netrw because we have nvim-tree
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors (24 bits color in TUI) to enable highlight groups
+vim.opt.termguicolors = true
+
+require('nvim-tree').setup()
 
