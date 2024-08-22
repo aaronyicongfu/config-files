ad1c6674bb4b1947b3526da0bebdbe7cf4551a67
Yicong Fu <fuyicong1996@gmail.com>
2024-08-22 12:07:04 -0400

    plugins for commenting and autofocus

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -57,7 +57,11 @@ local plugins = {
     -- trim trailing whitespace
     'cappyzawa/trim.nvim',
 
-    -- Hightlight word under cursor (TODO: find a useable plugin)
+    -- comment code
+    'numToStr/Comment.nvim',
+
+    -- auto-focus by resizing focused pane
+    'nvim-focus/focus.nvim'
 }
 
 -- Specify options
@@ -103,3 +107,40 @@ require('lualine').setup({
 
 -- set up trim trailing whitespace
 require('trim').setup()
+
+-- set up commenter
+require('Comment').setup()
+
+-- set up auto-focus and disable it for tree buffer:
+-- https://github.com/nvim-focus/focus.nvim?tab=readme-ov-file#disabling-focus
+require('focus').setup()
+local ignore_filetypes = { 'neo-tree' }
+local ignore_buftypes = { 'nofile', 'prompt', 'popup' }
+
+local augroup =
+    vim.api.nvim_create_augroup('FocusDisable', { clear = true })
+
+vim.api.nvim_create_autocmd('WinEnter', {
+    group = augroup,
+    callback = function(_)
+        if vim.tbl_contains(ignore_buftypes, vim.bo.buftype)
+        then
+            vim.w.focus_disable = true
+        else
+            vim.w.focus_disable = false
+        end
+    end,
+    desc = 'Disable focus autoresize for BufType',
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+    group = augroup,
+    callback = function(_)
+        if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+            vim.b.focus_disable = true
+        else
+            vim.b.focus_disable = false
+        end
+    end,
+    desc = 'Disable focus autoresize for FileType',
+})
